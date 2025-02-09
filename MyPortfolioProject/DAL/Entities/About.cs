460498752dbc4233e0452bf59c78e0c2947238d9
AbdussamedColdur <121455972+AbdussamedColdur@users.noreply.github.com>
2025-02-09 22:12:03 +0300

    UI parçalara ayırıldı. Veri tabanından verilerin çekilmesi ve dinamik hale getirilme işlemlerine başlandı.

---
+++ MyPortfolioProject/DAL/Entities/About.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolioProject.DAL.Entities
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string SubDescription{ get; set; }
+        public string Detail{ get; set; }
+    }
+}
---
+++ MyPortfolioProject/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioProject.DAL.Context;
+
+namespace MyPortfolioProject.ViewComponents
+{
+    public class _AboutComponentPartial: ViewComponent
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioContext.Abouts.ToList();
+            return View(values);
+        }
+    }
+}
